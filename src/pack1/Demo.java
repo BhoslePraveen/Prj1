f526aad7488dab96325549d884caf9e79e8161ad
praveen <praveen.bhosle5@gmail.com>
2024-09-27 23:24:59 +0530

    Added First commit

---
+++ src/pack1/Demo.java
@@ -0,0 +1,15 @@
+package pack1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Demo {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        set.add(12);
+        set.add(null);
+        set.add(11);
+        set.add(33);
+        System.out.println(set);
+    }
+}
